d314f73e735b4929730cf3155b3b6b53b43010ea
KL-Lru <kilattoeruru@outlook.jp>
2019-02-15 16:54:31 +0900

    Solve Problem 9

---
+++ Problem9.hs
@@ -0,0 +1,15 @@
+import Debug.Trace
+
+pythagorasProduct :: Int -> Int -> Int
+pythagorasProduct a b 
+  | isPythagoras a b c = a*b*c
+  | a < 1000 && b < 1000 = (pythagorasProduct (a+1) b)
+  | a == 1000 && b < 1000 = (pythagorasProduct 1 (b+1))
+  | otherwise = 0
+  where c = (1000-a-b)
+
+isPythagoras :: Int -> Int -> Int -> Bool
+isPythagoras a b c = (a^2 + b^2 == c^2)
+
+main = do
+  print $ pythagorasProduct 1 1
